a6d44ce183015cf16717277f058f663cc7837f2b
AnjaSvr <anjasvraka@gmail.com>
2021-04-07 14:15:45 +0200

    material

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ class App{
         //Replace Box with Circle, Cone, Cylinder, Dodecahedron, Icosahedron, Octahedron, Plane, Sphere, Tetrahedron, Torus or TorusKnot
         const geometry = new THREE.TorusKnotBufferGeometry(); 
         
-        const material = new THREE.MeshBasicMaterial( { color: 0xFF0000 });
+        const material = new THREE.MeshStandardMaterial( { color: 0x4f480 , metalness: 1, shininess: 60});
+        
 
         this.mesh = new THREE.Mesh( geometry, material );
         
